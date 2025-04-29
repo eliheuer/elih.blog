d425ea5ea9c11f59685752950953881b07a27766
Eli Heuer <elih@protonmail.com>
2025-04-29 00:46:19 -0700

    copy over cname file

--- src/consts.ts
+++ src/consts.ts
@@ -7,7 +7,7 @@ export const SITE: Site = {
   href: 'https://astro-erudite.vercel.app',
   author: 'jktrn',
   locale: 'en-US',
-  featuredPostCount: 5,
+  featuredPostCount: 3,
   postsPerPage: 3,
 }
 
