14b08e9e03feaefa24a8c49a8f2bed665f5a4d0a
Eli Heuer <elih@protonmail.com>
2025-04-28 22:43:16 -0700

    add graphics page

--- src/consts.ts
+++ src/consts.ts
@@ -17,6 +17,10 @@ export const NAV_LINKS: SocialLink[] = [
     label: 'projects',
   },
   {
+    href: '/graphics',
+    label: 'graphics',
+  },
+  {
     href: '/tags',
     label: 'tags',
   },
